2a54e2e651d830296686246bda3150699193aaee
Adytza Shermy <adytza1127@gmail.com>
2017-08-28 13:42:37 +0300

    second var added

--- HelloWorld/src/com/example/HelloWorld/HelloWorld.java
+++ HelloWorld/src/com/example/HelloWorld/HelloWorld.java
@@ -2,6 +2,7 @@ package com.example.HelloWorld;
 
 public class HelloWorld {
 	String path;
+	String timesVisited;
 	
 	public static void main(String[] args) {
 		System.out.println("hi hi hiiiii!!!");
